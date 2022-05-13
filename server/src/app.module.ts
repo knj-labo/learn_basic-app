7d4642dc2480deb03932aa385e1180f6b9e8ace8
jp-knj <kenji.tomita1996@gmail.com>
2022-05-13 23:09:55 +0900

    :sparkles: install module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TwitterService } from './twitter/twitter.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TwitterService],
 })
 export class AppModule {}
